6c70ff3fa35014ff5e1301cb880f61c8a1c145f6
Forrest <Forrest@Forrest-PC.router993eac.com>
2012-02-06 01:00:52 -0800

    Adding the correct set of mario files.

---
+++ src/devforrest/mario/core/GameFrame.java
@@ -0,0 +1,24 @@
+package devforrest.mario.core;
+
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame {
+	
+	public GameFrame() {	
+		
+		int w = 420;
+		int h = 330;       
+		setSize(w, h);
+		setResizable(false);
+		setTitle("Game Frame"); 
+		GamePanel panel = new GamePanel(w, h);
+		add(panel);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);	
+	}
+	
+	public static void main(String[] args) {
+		new GameFrame();
+	}
+
+}
